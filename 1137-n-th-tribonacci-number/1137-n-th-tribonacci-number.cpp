ccacfd9ff2e6f2e53ccb66ce86c0bf2b5d1b984d
Jinmu Go <jingmuio@gmail.com>
2026-06-09 08:06:25 +0900

    Time: 2 ms (3.83%), Space: 8.3 MB (37.79%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int tribonacci(int n) {
-      vector<int> dp;
-        dp.push_back(0);
-        dp.push_back(1);
-        dp.push_back(1);
-
-        if (n <= 2) {
-            return dp[n];
-        }
+        if (n == 0) {
+            return 0;
+        } else if (n <= 2) {
+            return 1;
+        } 
+      vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
 
         for (int i = 3; i < n; i++) {
-            dp.push_back(dp[i - 1] + dp[i - 2] + dp[i - 3]);
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
 
     return dp[n - 1] + dp[n - 2] + dp[n -3];
