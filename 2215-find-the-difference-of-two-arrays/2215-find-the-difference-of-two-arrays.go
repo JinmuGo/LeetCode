1aecbdaff3eba3056cc0d168a5dd766445e7c603
Jinmu Go <jingmuio@gmail.com>
2026-06-12 22:02:24 +0900

    Time: 8 ms (56.06%), Space: 8.9 MB (55.45%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.go
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.go
@@ -1,43 +1,25 @@
 import "fmt"
 
 func findDifference(nums1 []int, nums2 []int) [][]int {
-	map1 := make(map[int]bool)
-	map2 := make(map[int]bool)
+	hashMap := make(map[int]bool)
+    arr := make([][]int, 2)
 
-	for _, val := range nums1 {
-		map1[val] = true
-	}
+    for _, val := range nums1 {
+        hashMap[val] = true
+    }
 
-	for _, val := range nums2 {
-		map2[val] = true
-	}
+    for _, val := range nums2 {
+        if _, ok := hashMap[val]; !ok {
+            arr[1] = append(arr[1], val)
+        }
+        hashMap[val] = false
+    }
 
-	for _, val := range nums2 {
-		if map1[val] {
-			map1[val] = false
-		}
-	}
-
-	for _, val := range nums1 {
-		if map2[val] {
-			map2[val] = false
-		}
-	}
-
-	arr := make([][]int, 2)
-
-
-	for key, val := range map1 {
-		if val {
+    for key, val := range hashMap {
+        if val {
             arr[0] = append(arr[0], key)
-		}
-	}
-
-	for key, val := range map2 {
-		if val {
-            arr[1] = append(arr[1], key)
-		}
-}
+        }
+    }
 
 	return arr
 }
