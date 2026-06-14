9920a23b432082a3d9d21d2d962cbf57c77d28c6
Jinmu Go <jingmuio@gmail.com>
2026-06-14 16:54:13 +0900

    Time: 0 ms (100%), Space: 8.9 MB (25.46%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+    for root != nil {
+        if root.Val == val {
+            return root
+        }
+
+        if root.Val > val {
+            root = root.Left
+        } else {
+            root = root.Right
+        }
+    }
+
+    return root
+}
