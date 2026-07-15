343713a12f822f0407b936eef979e90ab4da6470
Jinmu Go <jingmuio@gmail.com>
2026-07-15 09:10:17 +0900

    Time: 0 ms (100%), Space: 4 MB (43.75%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.go
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.go
@@ -1,13 +1,3 @@
 func gcdOfOddEvenSums(n int) int {
-	sumOdd := n * n
-	sumEven := n * (n + 1)
-
-    return gcd(sumOdd, sumEven)
-}
-
-func gcd(a, b int) int {
-	for b != 0 {
-		a, b = b, a%b
-	}
-	return a
+	return n
 }
